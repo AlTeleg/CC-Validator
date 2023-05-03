07fd35524781cb2a7e372e3014f53238d75b061e
Александр <alteleg@yandex.ru>
2023-05-04 00:18:15 +0300

    Fixes

--- e2e/test.js
+++ e2e/test.js
@@ -17,7 +17,7 @@ describe('input tests', () => {
             })
         });
         browser = await puppeteer.launch({
-            headless: true
+            headless: false
         });
         page = await browser.newPage();
     })
